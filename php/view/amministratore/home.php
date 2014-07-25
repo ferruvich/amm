7977c8dd6493d4cb5b5f3175d76b06f9bf36fc6f
ferruvich <ferruvich@gmail.com>
2014-07-25 15:13:53 +0200

    progetto

---
+++ php/view/amministratore/home.php
@@ -0,0 +1,17 @@
+<h2 class="icon-title" id="h-home">Pannello di Controllo</h2>
+<p>
+    Benvenuto, Davide. 
+</p>
+<p>
+    Scegli una fra le seguenti sezioni:
+</p>
+<ul class="panel-mini">
+    <li><a href="amministratore/docenti_cerca" id="pnl-anagrafica">Docenti</a></li>
+    <li><a href="amministratore/studenti_cerca" id="pnl-anagrafica">Studenti</a></li>
+    <li><a id="pnl-dipartimenti" href="amministratore/dipartimenti">Dipartimenti</a></li>
+    <li><a id="pnl-dipartimenti" href="amministratore/cdl">Corsi di Laurea</a></li>
+    <li><a id="pnl-dipartimenti" href="amministratore/user">Nuovo Utente</a></li>
+    <li><a id="pnl-cerca" href="amministratore/esami">Elenco Esami</a></li>
+
+</ul>
+<div class="clear"></div>
