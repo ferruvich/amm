30558ed8df0170930cb77bcf045434f8e83bd660
ferruvich <ferruvich@gmail.com>
2014-07-25 16:14:56 +0200

    progetto

--- php/Settings.php
+++ php/Settings.php
@@ -9,9 +9,9 @@ class Settings {
 
     // variabili di accesso per il database
     public static $db_host = 'localhost';
-    public static $db_user = 'ferruDanielestef';
-    public static $db_password = 'upupa551';
-    public static $db_name='amm14_ferruDanielestef';
+    public static $db_user = 'esammi';
+    public static $db_password = 'davide';
+    public static $db_name='esammi';
     
     private static $appPath;
 
@@ -26,11 +26,11 @@ class Settings {
             switch ($_SERVER['HTTP_HOST']) {
                 case 'localhost':
                     // configurazione locale
-                    self::$appPath = 'http://' . $_SERVER['HTTP_HOST'] . '/amm/';
+                    self::$appPath = 'http://' . $_SERVER['HTTP_HOST'] . '/esAMM2014/esami14/';
                     break;
                 case 'spano.sc.unica.it':
                     // configurazione pubblica
-                    self::$appPath = 'http://' . $_SERVER['HTTP_HOST'] . '/amm2014/ferruDanielestef/amm/';
+                    self::$appPath = 'http://' . $_SERVER['HTTP_HOST'] . '/amm2014/davide/esami14/';
                     break;
 
                 default:
