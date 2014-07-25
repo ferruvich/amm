9011c1a36fd49ebb8c253a7d34c74050d0cccc86
ferruvich <ferruvich@gmail.com>
2014-07-25 15:48:32 +0200

    progetto

--- php/Settings.php
+++ php/Settings.php
@@ -11,7 +11,7 @@ class Settings {
     public static $db_host = 'localhost';
     public static $db_user = 'ferruDanielestef';
     public static $db_password = 'upupa551';
-    public static $db_name='amm14_ferrudanielestef';
+    public static $db_name='amm14_ferruDanielestef';
     
     private static $appPath;
 
@@ -26,14 +26,14 @@ class Settings {
             switch ($_SERVER['HTTP_HOST']) {
                 case 'localhost':
                     // configurazione locale
-                    self::$appPath = 'http://' . $_SERVER['HTTP_HOST'] . '/esAMM2014/esami14/';
+                    self::$appPath = 'http://' . $_SERVER['HTTP_HOST'] . '/amm/';
                     break;
                 case 'spano.sc.unica.it':
                     // configurazione pubblica
 <<<<<<< HEAD
-                    self::$appPath = 'http://' . $_SERVER['HTTP_HOST'] . '/amm2014/ferrudanielestef/esami2014';
+                    self::$appPath = 'http://' . $_SERVER['HTTP_HOST'] . '/amm/ferruDanielestef/esami2014';
 =======
-                    self::$appPath = 'http://' . $_SERVER['HTTP_HOST'] . '/amm2014/ferrudanielestef/amm14_ferruDanielestef/';
+                    self::$appPath = 'http://' . $_SERVER['HTTP_HOST'] . '/amm/ferruDanielestef/amm14_ferruDanielestef/';
 >>>>>>> afc96194e0ae66d5a6b0e0e5c4bc31788a6394b7
                     break;
 
