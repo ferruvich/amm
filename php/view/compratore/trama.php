0726f48febbcf2fc7edab66a35bf6af13910fd56
ferruvich <ferruvich@gmail.com>
2014-09-03 17:59:47 +0200

    errore

--- php/view/compratore/trama.php
+++
@@ -1,7 +0,0 @@
-<h2 class="icon-title" id="h-videogioco">Trama di <?= $copia->getVideogioco()->getTitolo()?></h2>
-
-<p>
-	 <?= $trama ?>
-	<br> <br>
-	<a href="compratore/acquisto<?= $vd->scriviToken('?')?>" id="pnl-iscrizione">Indietro</a>
-</p>
