e0bc41f2d832bf6782480d88b0bd309cc7471f37
ferruvich <ferruvich@gmail.com>
2014-09-03 18:08:40 +0200

    Parte view del MVC, compratore

---
+++ php/view/compratore/leftBar.php
@@ -0,0 +1,8 @@
+<h2 class="icon-title">Compratore</h2>
+<ul>
+    <li class="<?= $vd->getSottoPagina() == 'home' || $vd->getSottoPagina() == null ? 'current_page_item' : ''?>"><a href="compratore<?= $vd->scriviToken('?')?>">Home</a></li>
+    <li class="<?= $vd->getSottoPagina() == 'anagrafica' ? 'current_page_item' : '' ?>"><a href="compratore/anagrafica<?= $vd->scriviToken('?')?>">Anagrafica</a></li>
+    <li class="<?= $vd->getSottoPagina() == 'acquisto' ? 'current_page_item' : '' ?>"><a href="compratore/acquisto<?= $vd->scriviToken('?')?>">Compra un gioco!</a></li>
+	<li id="carrello"></li>
+	<li class="logout"><a href="compratore?cmd=logout">Logout</a></li>
+</ul>
