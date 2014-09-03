00c29fa486736e11ea1876d2189e11a15703b22c
ferruvich <ferruvich@gmail.com>
2014-09-03 18:05:20 +0200

    usato per il path dellapplicazione

---
+++ php/Settings.php
@@ -0,0 +1,36 @@
+<?php
+
+class Settings {
+
+    // variabili di accesso per il database
+    public static $db_host = 'localhost';
+    public static $db_user = 'ferruvich';
+    public static $db_password = 'upupa551';
+    public static $db_name = 'amm14_ferruDanielestef';
+    
+    private static $appPath;
+
+    public static function getApplicationPath() {
+        if (!isset(self::$appPath)) {
+            // restituisce il server corrente
+            switch ($_SERVER['HTTP_HOST']) {
+                case 'localhost':
+                    // configurazione locale
+                    self::$appPath = 'http://' . $_SERVER['HTTP_HOST'] . '/amm/';
+                    break;
+                case 'spano.sc.unica.it':
+                    // configurazione pubblica
+                    self::$appPath = 'http://' . $_SERVER['HTTP_HOST'] . '/amm2014/ferruDanieleStef/amm/';
+                    break;
+                default:
+                    self::$appPath = '';
+                    break;
+            }
+        }
+        
+        return self::$appPath;
+    }
+
+}
+
+?>
