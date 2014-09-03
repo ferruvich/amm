ef66fc070280b980a31f2d61d6f1d9b3dd8b805c
ferruvich <ferruvich@gmail.com>
2014-09-03 18:06:26 +0200

    controller degli utenti

---
+++ php/controller/BaseController.php
@@ -0,0 +1,215 @@
+<?php
+
+include_once basename(__DIR__) . '/../view/ViewDescriptor.php';
+include_once basename(__DIR__) . '/../model/User.php';
+include_once basename(__DIR__) . '/../model/UserFactory.php';
+
+class BaseController {
+
+    const user = 'user';
+    const role = 'role';
+    const impersonato = '_imp';
+
+    public function __construct() {
+        
+    }
+
+
+    public function handleInput(&$request) {
+        $vd = new ViewDescriptor();
+
+        $vd->setPagina($request['page']);
+
+        $this->setImpToken($vd, $request);
+
+
+        if (isset($request["cmd"])) {
+            switch ($request["cmd"]) {
+                case 'login':
+                    $username = isset($request['user']) ? $request['user'] : '';
+                    $password = isset($request['password']) ? $request['password'] : '';
+                    $this->login($vd, $username, $password);
+                    if ($this->loggedIn())
+                        $user = UserFactory::instance()->cercaUtentePerId($_SESSION[self::user], $_SESSION[self::role]);
+                    break;
+                default : $this->showLoginPage();
+            }
+        } else {
+            if ($this->loggedIn()) {
+                $user = UserFactory::instance()->cercaUtentePerId($_SESSION[self::user], $_SESSION[self::role]);
+
+                $this->showHomeUtente($vd);
+            } else {
+                $this->showLoginPage($vd);
+            }
+        }
+
+        require basename(__DIR__) . '/../view/master.php';
+    }
+
+    protected function loggedIn() {
+        return isset($_SESSION) && array_key_exists(self::user, $_SESSION);
+    }
+
+    protected function showLoginPage($vd) {
+        // mostro la pagina di login
+        $vd->setTitolo("God of Videogame");
+        $vd->setMenuFile(basename(__DIR__) . '/../view/login/menu.php');
+        $vd->setLogoFile(basename(__DIR__) . '/../view/login/logo.php');
+        $vd->setLeftBarFile(basename(__DIR__) . '/../view/login/leftBar.php');
+        $vd->setContentFile(basename(__DIR__) . '/../view/login/content.php');
+    }
+
+    protected function showHomeCompratore($vd) {
+        $vd->setTitolo("God of Videogame");
+        $vd->setMenuFile(basename(__DIR__) . '/../view/compratore/menu.php');
+        $vd->setLogoFile(basename(__DIR__) . '/../view/compratore/logo.php');
+        $vd->setLeftBarFile(basename(__DIR__) . '/../view/compratore/leftBar.php');
+        $vd->setContentFile(basename(__DIR__) . '/../view/compratore/content.php');
+    }
+    protected function showHomeVenditore($vd) {
+        $vd->setTitolo("God of Videogame");
+        $vd->setMenuFile(basename(__DIR__) . '/../view/venditore/menu.php');
+        $vd->setLogoFile(basename(__DIR__) . '/../view/venditore/logo.php');
+        $vd->setLeftBarFile(basename(__DIR__) . '/../view/venditore/leftBar.php');
+        $vd->setRightBarFile(basename(__DIR__) . '/../view/venditore/rightBar.php');
+        $vd->setContentFile(basename(__DIR__) . '/../view/venditore/content.php');
+    }
+
+    protected function showHomeUtente($vd) {
+        $user = UserFactory::instance()->cercaUtentePerId($_SESSION[self::user], $_SESSION[self::role]);
+        switch ($user->getRuolo()) {
+            case User::Compratore:
+                $this->showHomeCompratore($vd);
+                break;
+
+            case User::Venditore:
+                $this->showHomeVenditore($vd);
+                break;
+        }
+    }
+
+    protected function setImpToken(ViewDescriptor $vd, &$request) {
+
+        if (array_key_exists('_imp', $request)) {
+            $vd->setImpToken($request['_imp']);
+        }
+    }
+
+    protected function login($vd, $username, $password) {
+        // carichiamo i dati dell'utente
+
+        $user = UserFactory::instance()->caricaUtente($username, $password);
+        if (isset($user) && $user->esiste()) {
+            // utente autenticato
+            $_SESSION[self::user] = $user->getId();
+            $_SESSION[self::role] = $user->getRuolo();
+            $this->showHomeUtente($vd);
+        } else {
+            $vd->setMessaggioErrore("Utente sconosciuto o password errata");
+            $this->showLoginPage($vd);
+        }
+    }
+
+    protected function logout($vd) {
+        // reset array $_SESSION
+        $_SESSION = array();
+        // termino la validita' del cookie di sessione
+        if (session_id() != '' || isset($_COOKIE[session_name()])) {
+            // imposto il termine di validita' al mese scorso
+            setcookie(session_name(), '', time() - 2592000, '/');
+        }
+        // distruggo il file di sessione
+        session_destroy();
+        $this->showLoginPage($vd);
+    }
+    protected function aggiornaIndirizzo($user, &$request, &$msg) {
+
+        if (isset($request['via'])) {
+            if (!$user->setVia($request['via'])) {
+                $msg[] = '<li>La via specificata non &egrave; corretta</li>';
+            }
+        }
+        if (isset($request['civico'])) {
+            if (!$user->setNumeroCivico($request['civico'])) {
+                $msg[] = '<li>Il formato del numero civico non &egrave; corretto</li>';
+            }
+        }
+        if (isset($request['citta'])) {
+            if (!$user->setCitta($request['citta'])) {
+                $msg[] = '<li>La citt&agrave; specificata non &egrave; corretta</li>';
+            }
+        }
+        if (isset($request['provincia'])) {
+            if (!$user->setProvincia($request['provincia'])) {
+                $msg[] = '<li>La provincia specificata &egrave; corretta</li>';
+            }
+        }
+        if (isset($request['cap'])) {
+            if (!$user->setCap($request['cap'])) {
+                $msg[] = '<li>Il CAP specificato non &egrave; corretto</li>';
+            }
+        }
+
+        // salviamo i dati se non ci sono stati errori
+        if (count($msg) == 0) {
+            if (UserFactory::instance()->salva($user) != 1) {
+                $msg[] = '<li>Salvataggio non riuscito</li>';
+            }
+        }
+    }
+
+    protected function aggiornaEmail($user, &$request, &$msg) {
+        if (isset($request['email'])) {
+            if (!$user->setEmail($request['email'])) {
+                $msg[] = '<li>L\'indirizzo email specificato non &egrave; corretto</li>';
+            }
+        }
+        
+        // salviamo i dati se non ci sono stati errori
+        if (count($msg) == 0) {
+            if (UserFactory::instance()->salva($user) != 1) {
+                $msg[] = '<li>Salvataggio non riuscito</li>';
+            }
+        }
+    }
+
+    protected function aggiornaPassword($user, &$request, &$msg) {
+        if (isset($request['pass1']) && isset($request['pass2'])) {
+            if ($request['pass1'] == $request['pass2']) {
+                if (!$user->setPassword($request['pass1'])) {
+                    $msg[] = '<li>Il formato della password non &egrave; corretto</li>';
+                }
+            } else {
+                $msg[] = '<li>Le due password non coincidono</li>';
+            }
+        }
+        
+        // salviamo i dati se non ci sono stati errori
+        if (count($msg) == 0) {
+            if (UserFactory::instance()->salva($user) != 1) {
+                $msg[] = '<li>Salvataggio non riuscito</li>';
+            }
+        }
+    }
+
+    protected function creaFeedbackUtente(&$msg, $vd, $okMsg) {
+        if (count($msg) > 0) {
+            // ci sono messaggi di errore nell'array,
+            // qualcosa e' andato storto...
+            $error = "Si sono verificati i seguenti errori \n<ul>\n";
+            foreach ($msg as $m) {
+                $error = $error . $m . "\n";
+            }
+            // imposto il messaggio di errore
+            $vd->setMessaggioErrore($error);
+        } else {
+            // non ci sono messaggi di errore, la procedura e' andata
+            // quindi a buon fine, mostro un messaggio di conferma
+            $vd->setMessaggioConferma($okMsg);
+        }
+    }
+
+}
+
+?>
