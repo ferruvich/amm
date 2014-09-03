f68e3b33e931e0cd49faa54efa81ff5da3cfc317
ferruvich <ferruvich@gmail.com>
2014-09-03 18:29:50 +0200

    Update Settings.php

--- php/Settings.php
+++ php/Settings.php
@@ -20,7 +20,7 @@ class Settings {
                     break;
                 case 'spano.sc.unica.it':
                     // configurazione pubblica
-                    self::$appPath = 'http://' . $_SERVER['HTTP_HOST'] . '/amm2014/ferruDanieleStef/';
+                    self::$appPath = 'http://' . $_SERVER['HTTP_HOST'] . '/amm2014/ferruDanielestef/';
                     break;
                 default:
                     self::$appPath = '';
