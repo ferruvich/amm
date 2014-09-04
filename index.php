f67c5537586e5370a21a17b2678fdb153ad9c949
ferruvich <ferruvich@gmail.com>
2014-09-04 12:32:50 +0200

    Pagina di spiegazione

---
+++ index.php
@@ -0,0 +1,71 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta http-equiv="Content-Type" content="text-html; charset=UTF-8"/>
+		<title>Spiegazione progetto</title>
+	</head>
+	<body>
+		<h1>Spiegazione progetto </h1>
+		<h3>Nome: Daniele Stefano Ferru</h3>
+		<h3>Matricola: 47783</h3>
+
+		<p>	
+			Il progetto simula un sito di compravendita di videogiochi.
+			Sono presenti due tipi di utenti:
+			<ul>
+				<li>Venditore</li>
+				<li>Compratore</li>
+			</ul>
+
+			Il venditore può vendere copie di videogiochi esistenti nel database, oppure, nel caso i videogiochi non
+			siano presenti, li può aggiungere. <br>
+			Inoltre può verificare quante e quali copie sono ancora in vendita e in attesa di essere
+			comprate, e quante e quali copie sono state comprate, e quale utente ha comprato suddetta copia.<br> <br> <br>
+			Il compratore può semplicemente visualizzare la lista delle copie in vendita, e scegliere il videogioco che gli interessa e comprarlo.
+			<br>
+			La struttura del database è la seguente:
+		</p>
+		<img title="database" alt="struttura del DB" src="images/db.png"/>
+		<p>
+			<h4>Requisiti rispettati per il progetto:</h4>
+			<ul>
+				<li>Utilizzo di HTML e CSS: OK</li>
+				<li>Utilizzo di PHP e Mysql: OK</li>
+				<li>Utilizzo del pattern MVC: OK</li>
+				<li>Due ruoli: OK, venditore e compratore</li>
+				<li>Transazione: Non utilizzata</li>
+				<li>Utilizzo di js e ajax: Non utilizzato</li>
+			</ul>
+		</p>
+
+		<p>
+			<h4>Accesso alla pagina del progetto</h4>
+	
+			La homepage del progetto si trova sulla URL <a href="php/login">http://spano.sc.unica.it/amm2014/ferruDanielestef/php/login</a>
+			<br>
+			Ci si può accedere con le seguenti credenziali
+			
+			<ul>
+				<li>Venditore
+					<ul>
+						<li>Username: anfen</li>
+						<li>Password: venditore</li>
+					</ul>
+				</li>
+				<li>Compratore
+					<ul>
+						<li>Username: ferruvich</li>
+						<li>Password: compratore</li>
+					</ul>
+				</li>
+			</ul>
+		</p>
+		<p>
+			<h4>Link utili</h4>
+			Di seguito il link del repository github:
+			<a href="http://github.com/ferruvich/amm">http://github.com/ferruvich/amm</a>
+		</p>
+		<br>
+		<br>
+	</body>
+</html>
